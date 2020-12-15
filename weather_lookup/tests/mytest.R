679b8e0b74e0a39a773548a78ac7cd57b92a7f8a
Nick Strayer <nick.strayer@gmail.com>
2020-12-15 16:39:30 -0500

    Flushed out test a bit to use seeds and save benchmark object

--- weather_lookup/tests/mytest.R
+++ weather_lookup/tests/mytest.R
@@ -6,25 +6,29 @@ run_use_test <- function(use_caching){
   # Make sure you have the latest version of shinytest from github so option passing
   # is available
   app <- ShinyDriver$new("./", phantomTimeout = 10000, loadTimeout = 10000,
-                         options = list(cache = use_caching))
+                         options = list(cache = use_caching),
+                         seed = 42)
+  # Using a seed because the app randomly chooses city with rnd_city button
   app$snapshotInit("mytest")
 
   app$snapshot()
-  app$setInputs(city = "Redwood City, CA")
-  app$setInputs(prev_city = "click")
-  app$setInputs(rnd_city = "click")
-  app$setInputs(prev_city = "click")
-  app$setInputs(rnd_city = "click")
-  app$setInputs(prev_city = "click")
-  app$setInputs(prev_city = "click")
-  app$setInputs(prev_city = "click")
+  app$setInputs(city = "Redwood City, CA", timeout_ = 10000)
+  app$setInputs(prev_city = "click"      , timeout_ = 10000)
+  app$setInputs(rnd_city = "click"       , timeout_ = 10000)
+  app$setInputs(prev_city = "click"      , timeout_ = 10000)
+  app$setInputs(rnd_city = "click"       , timeout_ = 10000)
+  app$setInputs(prev_city = "click"      , timeout_ = 10000)
+  app$setInputs(prev_city = "click"      , timeout_ = 10000)
+  app$setInputs(prev_city = "click"      , timeout_ = 10000)
   app$snapshot()
 }
 
 
-bench::mark(
+speed_comparison <- bench::mark(
   run_use_test(use_caching = TRUE),
   run_use_test(use_caching = FALSE),
   iterations = 5,
   check = FALSE
 )
+
+speed_comparison
