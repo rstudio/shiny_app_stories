f9c33c2237500f4ff36f84aff221be56a879fcf3
Nick Strayer <nick.strayer@gmail.com>
2020-12-15 18:34:52 -0500

    Added name column to station directory df

--- weather_lookup/data/gather_station_info.R
+++ weather_lookup/data/gather_station_info.R
@@ -28,14 +28,14 @@ zip_db_cols <- c(
 zip_to_city <- read_csv(
   here('data/zip_code_database.csv'),
   col_types = zip_db_cols
-) %>% 
-  select(zip, primary_city, acceptable_cities, state, latitude, longitude) %>% 
+) %>%
+  select(zip, primary_city, acceptable_cities, state, latitude, longitude) %>%
   pivot_longer(
     cols = c(primary_city, acceptable_cities),
     values_to = "city"
-  ) %>% 
-  filter(!is.na(city)) %>% 
-  distinct(zip, city, state, .keep_all = TRUE) %>% 
+  ) %>%
+  filter(!is.na(city)) %>%
+  distinct(zip, city, state, .keep_all = TRUE) %>%
   transmute(
     zip = str_pad(zip, 5, pad = "0"),
     city = paste0(city, ", ", state)
@@ -46,13 +46,12 @@ station_zip_codes <- read_table(
   col_names = c("station", 'zip', 'name')
 )
 
-# Merge two datasets together and keep only a single station per city. 
-# It may be better to check the data to choose the "best" station for 
+# Merge two datasets together and keep only a single station per city.
+# It may be better to check the data to choose the "best" station for
 # a given city, but that seems like a lot of work
-station_to_city <- station_zip_codes %>% 
-  left_join(zip_to_city, by = "zip") %>% 
-  filter(!is.na(city)) %>% 
-  select(-name)
+station_to_city <- station_zip_codes %>%
+  left_join(zip_to_city, by = "zip") %>%
+  filter(!is.na(city))
 
 write_rds(station_to_city, here("data/station_to_city.rds"))
 
