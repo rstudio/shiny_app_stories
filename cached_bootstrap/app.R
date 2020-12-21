b318586281c055ee1d68dcecd3e32247fdca63f0
Nick Strayer <nick.strayer@gmail.com>
2020-12-21 18:59:01 -0500

    Switched to manually building bootstrap and adding lines to plot

--- cached_bootstrap/app.R
+++ cached_bootstrap/app.R
@@ -1,63 +1,63 @@
 # install.packages('simpleboot')
-# devtools::install_github("ropensci/skimr")
 library(shiny)
-library(tidyverse)
-library(simpleboot)
-library(rlang)
-library(glue)
-
-data("midwest")
+library(dplyr)
+library(ggplot2)
+library(bslib)
 
 # Log all the population variables
-midwest <- midwest %>%
+midwest <- ggplot2::midwest %>%
   select(-PID) %>%
   mutate(across(contains("pop"), log10))
 
 numeric_vars <- midwest %>% select_if(is.numeric) %>% colnames()
 
-
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-    titlePanel("Bootstrapped Regressions"),
-    sidebarLayout(
-        sidebarPanel(
-          selectInput('x_var', "X variable", choices = numeric_vars, selected = "popdensity"),
-          selectInput('y_var', "Y variable", choices = numeric_vars, selected = "percollege")
-        ),
-        mainPanel(
-           plotOutput("scatter"),
-           textOutput('boot_results')
-        )
+  theme = bslib::bs_theme(bootswatch = "flatly"),
+  titlePanel("Bootstrapped Regressions"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput('x_var', "X variable", choices = numeric_vars, selected = "popdensity"),
+      selectInput('y_var', "Y variable", choices = numeric_vars, selected = "percollege")
+    ),
+    mainPanel(
+      uiOutput('boot_results'),
+      plotOutput("scatter")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$scatter <- renderPlot({
-      midwest %>%
-        ggplot(aes(x = .data[[input$x_var]], y = .data[[input$y_var]])) +
-        geom_point()
+  output$scatter <- renderPlot({
+    ggplot(midwest, aes(x = .data[[input$x_var]], y = .data[[input$y_var]])) +
+      geom_abline(data = bootstrap_results(), aes(intercept = intercept, slope = effect_size), alpha = 0.1, color = "steelblue") +
+      geom_point() +
+      labs(subtitle = paste("Each blue line represents one of the", B, "bootstrap regression results"))
+  }) %>%
+    bindCache(input$y_var, input$x_var)
+
+  bootstrap_results <- reactive({
+    validate(need(input$y_var != input$x_var, "X and Y variables need to be different."))
+    effect_sizes <- tibble(intercept = numeric(), effect_size = numeric())
+
+    B <- 500 # Number of bootstrap resamples to do
+    withProgress(message = 'Bootstrap resampling', max = B, {
+      for(i in 1:B){
+        if((i %% 50) == 0) incProgress(i, paste("Itteration", i))
+        boot_data <- sample_frac(midwest, replace = TRUE)
+        effect_sizes[i,] <- as.list(lm(boot_data[[input$y_var]] ~ boot_data[[input$x_var]])$coefficients)
+      }
     })
+    effect_sizes
+  }) %>%
+    bindCache(input$y_var, input$x_var)
 
-    bootstrap_results <- reactive({
-      validate(
-        need(input$y_var != input$x_var,
-             "Need x and y variables to be different to perform regression.")
-      )
-      dependent <- midwest[[input$y_var]]
-      independent <- midwest[[input$x_var]]
-      lm.boot(lm(dependent ~ independent), R = 500)
-    }) %>%
-      bindCache(input$y_var, input$x_var)
-
-    output$boot_results <- renderText({
-      effect_size <- summary(bootstrap_results()$orig.lm)$coefficients[2,1]
-      bs_se_w <- 1.96 * summary(bootstrap_results())$stdev.params["independent"]
-      glue("Impact of increasing {input$x_var} by one unit is a change of {input$y_var} by ",
-           "{round(effect_size - bs_se_w,3)} to {round(effect_size + bs_se_w, 3)}")
-    })
-
+  output$boot_results <- renderUI({
+    effect_size <- round(quantile(bootstrap_results()$effect_size, c(0.025, 0.975)), 3)
+    markdown(paste0("Impact of increasing `", input$x_var, "` by one unit is a change of `",
+                    input$y_var, "` is __", effect_size[1], "__ to __", effect_size[2], "__"))
+  })
 }
 
 # Run the application
